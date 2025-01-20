a67b67d45c6dca662e5e1482e56e24e588e2b974
Mohammad Saleh Abbasi <abbasisaleh895@gmail.com>
2025-01-19 21:26:29 -0800

    added js18

---
+++ JS-18-Q4.js
@@ -0,0 +1,36 @@
+/*
+  Write a function that calculates the sum of two numbers.
+
+  - The function must return a promise.
+  - The solution should be solved after half a second.
+  - If a negative value is entered into the function, it will be rejected.
+  - Note: you should use "Promise" to resolve this question.
+*/
+
+function sum(num1, num2) {
+  return new Promise((resolve, reject) => {
+    if (num1 < 0 || num2 < 0) {
+      reject('Negative values are not allowed.');
+      return; 
+    }
+    setTimeout(() => {
+      const result = num1 + num2; 
+      resolve(result); 
+    }, 500);
+  });
+}
+sum(5, 3)
+  .then(result => {
+    console.log('Sum:', result); 
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+sum(-1, 3)
+  .then(result => {
+    console.log('Sum:', result);
+  })
+  .catch(error => {
+    console.error('Error:', error); 
+  });
